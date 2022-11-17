c5c393b7ce10d8b2329b59475f296d76d0db94ad
Florian Scholz <florian.scholz@spryker.com>
2022-11-17 15:37:37 +0100

    Data Transfer Object - Skeleton

--- src/Pyz/Zed/Training/Communication/Controller/HelloController.php
+++ src/Pyz/Zed/Training/Communication/Controller/HelloController.php
@@ -8,7 +8,10 @@ class HelloController extends AbstractController
 {
     public function indexAction()
     {
+        // TODO: initialize the antelope DTO and set a name
+
         return $this->viewResponse([
+            // TODO: pass the DTO to the view
             'helloWorldText' => 'Hello World!'
         ]);
     }
