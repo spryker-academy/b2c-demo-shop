0f43f0808b1421087ef22ef735b555b56c86952d
Florian Scholz <florian.scholz@spryker.com>
2022-11-17 15:22:34 +0100

    Hello World Back Office - Skeleton

---
+++ src/Pyz/Zed/Training/Communication/Controller/HelloController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pyz\Zed\Training\Communication\Controller;
+
+use Spryker\Zed\Kernel\Communication\Controller\AbstractController;
+
+class HelloController extends AbstractController
+{
+    public function indexAction()
+    {
+        // TODO
+    }
+}
