a2ac6e4d7ec168c42e022d94550cfd679cdc2b98
Florian Schwarzmeier <Florian.Schwarzmeier@spryker.com>
2022-12-15 12:37:10 +0100

    Module Layers - Skeleton

--- src/Pyz/Yves/Router/RouterDependencyProvider.php
+++ src/Pyz/Yves/Router/RouterDependencyProvider.php
@@ -8,6 +8,7 @@
 namespace Pyz\Yves\Router;
 
 use Pyz\Yves\ExampleProductSalePage\Plugin\Router\ExampleProductSaleRouteProviderPlugin;
+use Pyz\Yves\TrainingPage\Plugin\Router\TrainingPageRouteProviderPlugin;
 use Spryker\Yves\HealthCheck\Plugin\Router\HealthCheckRouteProviderPlugin;
 use Spryker\Yves\Router\Plugin\RouteManipulator\LanguageDefaultPostAddRouteManipulatorPlugin;
 use Spryker\Yves\Router\Plugin\RouteManipulator\SslPostAddRouteManipulatorPlugin;
@@ -112,6 +113,7 @@ class RouterDependencyProvider extends SprykerRouterDependencyProvider
             new OrderCancelWidgetRouteProviderPlugin(),
             new PaymentPageRouteProviderPlugin(),
             new ProductConfiguratorGatewayPageRouteProviderPlugin(),
+            // TODO: Add the RouteProviderPlugin of the TrainingPage module
         ];
     }
 
