e764c03394b86fc62c21e3233e2ca6ef3914b802
Florian Schwarzmeier <florian.schwarzmeier@spryker.com>
2022-12-21 11:26:52 +0100

    Extending Core Modules - Skeleton

--- src/Pyz/Zed/Training/Communication/Controller/GatewayController.php
+++ src/Pyz/Zed/Training/Communication/Controller/GatewayController.php
@@ -3,6 +3,7 @@
 namespace Pyz\Zed\Training\Communication\Controller;
 
 use Generated\Shared\Transfer\AntelopeCriteriaTransfer;
+use Generated\Shared\Transfer\AntelopeTransfer;
 use Spryker\Zed\Kernel\Communication\Controller\AbstractGatewayController;
 
 /**
@@ -14,4 +15,7 @@ class GatewayController extends AbstractGatewayController
     {
         return $this->getFacade()->findAntelope($antelopeCriteria);
     }
+
+    // TODO: Add method for creating an antelope by using the facade
+    // Hint: The method should accept an AntelopeTransfer for the needed antelope data as parameter
 }
