f54f0e7158d8de6f7aef25fc6653ad3559aeaf26
Florian Schwarzmeier <Florian.Schwarzmeier@spryker.com>
2023-03-30 11:39:58 +0200

    Data Import - Complete

--- src/Pyz/Zed/AntelopeDataImport/Business/AntelopeDataImportFacade.php
+++ src/Pyz/Zed/AntelopeDataImport/Business/AntelopeDataImportFacade.php
@@ -23,8 +23,8 @@ class AntelopeDataImportFacade extends AbstractFacade implements AntelopeDataImp
     public function importAntelope(
         ?DataImporterConfigurationTransfer $dataImporterConfigurationTransfer = null,
     ): DataImporterReportTransfer {
-        // TODO: Use the factory to get the AntelopeDataImport, call the `import()`-method and return its result
-        // Hint-1: You can access the AntelopeDataImportBusinessFactory through $this->getFactory()
-        // Hint-2: Do not forget to pass the DataImporterConfigurationTransfer to BOTH methods
+        return $this->getFactory()
+            ->getAntelopeDataImport($dataImporterConfigurationTransfer)
+            ->import($dataImporterConfigurationTransfer);
     }
 }
