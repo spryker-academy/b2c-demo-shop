9d8f7359b2149a514a45521a55dc27159bffe3cf
Florian Schwarzmeier <Florian.Schwarzmeier@spryker.com>
2023-06-07 18:18:00 +0200

    Search - Skeleton

---
+++ src/Pyz/Yves/AntelopePage/AntelopePageFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Pyz\Yves\AntelopePage;
+
+use Pyz\Client\AntelopeSearch\AntelopeSearchClientInterface;
+use Spryker\Yves\Kernel\AbstractFactory;
+
+class AntelopePageFactory extends AbstractFactory
+{
+    // TODO-1: Create the `getAntelopeSearchClient` method here - The return type should be `AntelopeSearchClientInterface`
+    // Hint-1: Use the getProvidedDependency() method and provide the key you used in the AntelopePageDependencyProvider class: CLIENT_ANTELOPE_SEARCH
+}
