6660a4605022b21a6f1a45bcc2f922c83eea075b
Florian Schwarzmeier <florian.schwarzmeier@spryker.com>
2023-06-27 12:14:04 +0200

    Glue Storefront API - Skeleton

---
+++ src/Pyz/Glue/AntelopesApi/Controller/AntelopesResourceController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Pyz\Glue\AntelopesApi\Controller;
+
+use Generated\Shared\Transfer\GlueRequestTransfer;
+use Generated\Shared\Transfer\GlueResponseTransfer;
+use Spryker\Glue\Kernel\Controller\AbstractStorefrontApiController;
+
+/**
+ * @method \Pyz\Glue\AntelopesApi\AntelopesApiFactory getFactory()
+ */
+class AntelopesResourceController extends AbstractStorefrontApiController
+{
+    /**
+     * @Glue({
+     *     "getResourceById": {
+     *          "summary": [
+     *              "Retrieves antelope by name."
+     *          ],
+     *          "parameters": [
+     *              {
+     *                  "ref": "acceptLanguage"
+     *              },
+     *              {
+     *                  "ref": "ContentType"
+     *              },
+     *              {
+     *                  "ref": "Fields"
+     *              }
+     *          ],
+     *          "responseAttributesClassName": "Generated\\Shared\\Transfer\\AntelopesApiAttributesTransfer",
+     *          "responses": {
+     *              "400": "Bad Request",
+     *              "403": "Unauthorized request",
+     *              "404": "Not Found"
+     *          }
+     *     }
+     * })
+     *
+     * @param \Generated\Shared\Transfer\GlueRequestTransfer $glueRequestTransfer
+     *
+     * @return \Generated\Shared\Transfer\GlueResponseTransfer
+     */
+    public function getAction(GlueRequestTransfer $glueRequestTransfer): GlueResponseTransfer
+    {
+        // TODO-1: Use the factory to create the AntelopeReader and return an Antelope-GlueResponse
+        // Hint-1: The method `getAntelope()` requires a GlueRequestTransfer and returns a GlueResponseTransfer
+    }
+}
