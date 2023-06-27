cda5cdf3ac6d6d7f68dd1a73c13c2eba1aa1e637
Florian Schwarzmeier <florian.schwarzmeier@spryker.com>
2023-06-27 15:32:14 +0200

    Glue Storefront API - Complete

--- src/Pyz/Glue/GlueStorefrontApiApplication/GlueStorefrontApiApplicationDependencyProvider.php
+++ src/Pyz/Glue/GlueStorefrontApiApplication/GlueStorefrontApiApplicationDependencyProvider.php
@@ -79,8 +79,7 @@ class GlueStorefrontApiApplicationDependencyProvider extends SprykerGlueStorefro
     {
         return [
             new OauthApiTokenResource(),
-            // TODO-1: Register the AntelopesResourcePlugin
-            // Hint-1: The plugin-stack expects an INSTANCE of the plugin
+            new AntelopesResourcePlugin(),
         ];
     }
 
