3c9d7ced0a941965ad8e46bf31f86c3ff604c003
Florian Schwarzmeier <florian.schwarzmeier@spryker.com>
2023-06-27 15:32:14 +0200

    Glue Storefront API - Complete

--- src/Pyz/Glue/AntelopesApi/Processor/Reader/AntelopeReader.php
+++ src/Pyz/Glue/AntelopesApi/Processor/Reader/AntelopeReader.php
@@ -39,23 +39,18 @@ class AntelopeReader
         $glueResponseTransfer = new GlueResponseTransfer();
         $antelopeName = $glueRequestTransfer->getResourceOrFail()->getIdOrFail();
 
-        // TODO-1: Use the AntelopeSearchClient to get an Antelope by its name
-        // Hint-1: Check the properties for accessing the AntelopeSearchClient
-        $antelopeTransfer = null;
+        $antelopeTransfer = $this->antelopeSearchClient->getAntelopeByName($antelopeName);
 
-        // TODO-2: Return an AntelopeNotFound-Error if there is no Antelope with that name
-        // Hint-1: You can use the method `addAntelopeNotFoundError()` of the current class
+        if (!$antelopeTransfer) {
+            return $this->addAntelopeNotFoundError($glueResponseTransfer);
+        }
 
-        // TODO-3: Use the AntelopeMapper to map the AntelopeTransfer to AntelopesApiAttributesTransfer
-        // Hint-1: Check the properties for accessing the AntelopeSearchClient
-        $antelopesApiAttributesTransfer = null;
+        $antelopesApiAttributesTransfer = $this->antelopeMapper->mapAntelopeTransferToAntelopesApiAttributesTransfer($antelopeTransfer);
 
         $glueResourceTransfer = (new GlueResourceTransfer())
-            ->setId($antelopesApiAttributesTransfer->getNameOrFail());
-
-        // TODO-4: Set the type of the resource we will return and set the AntelopesApiAttributesTransfer
-        // Hint-1: Check the class AntelopesApiConfig for the right constant
-        // Hint-2: The methods you are looking for are `setType()` and `setAttributes`
+            ->setId($antelopesApiAttributesTransfer->getNameOrFail())
+            ->setType(AntelopesApiConfig::RESOURCE_ANTELOPES)
+            ->setAttributes($antelopesApiAttributesTransfer);
 
         $glueResponseTransfer->addResource($glueResourceTransfer);
 
