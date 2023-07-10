f40fa92f72a5f5a18e8218d8ee93329969abca93
Florian Schwarzmeier <florian.schwarzmeier@spryker.com>
2023-07-10 18:23:30 +0200

    Order Management System - Skeleton

--- config/Shared/common/config_oms-development.php
+++ config/Shared/common/config_oms-development.php
@@ -40,14 +40,18 @@ $config[NopaymentConstants::WHITELIST_PAYMENT_METHODS] = [
 $config[OmsConstants::ACTIVE_PROCESSES] = array_merge([
     'Nopayment01',
     'DummyPayment01',
+    // TODO-1: Add our newly created Order Management State Machine to the active processes
+    // Hint-1: The name must match the file-name of the State Machine without file ending
 ], $config[OmsConstants::ACTIVE_PROCESSES]);
 
 $config[SalesConstants::PAYMENT_METHOD_STATEMACHINE_MAPPING] = array_replace(
     $config[SalesConstants::PAYMENT_METHOD_STATEMACHINE_MAPPING],
     [
-    DummyPaymentConfig::PAYMENT_METHOD_INVOICE => 'DummyPayment01',
-    DummyPaymentConfig::PAYMENT_METHOD_CREDIT_CARD => 'DummyPayment01',
-    NopaymentConfig::PAYMENT_PROVIDER_NAME => 'Nopayment01',
-    GiftCardConfig::PROVIDER_NAME => 'DummyPayment01',
+        // TODO-2: Replace the Order Management State Machine used for the invoice payment method with our newly created one
+        // Hint-1: The key `DummyPaymentConfig::PAYMENT_METHOD_INVOICE` should contain a different string value after the change
+        DummyPaymentConfig::PAYMENT_METHOD_INVOICE => 'DummyPayment01',
+        DummyPaymentConfig::PAYMENT_METHOD_CREDIT_CARD => 'DummyPayment01',
+        NopaymentConfig::PAYMENT_PROVIDER_NAME => 'Nopayment01',
+        GiftCardConfig::PROVIDER_NAME => 'DummyPayment01',
     ],
 );
